c528fd260e125a747fe6a8a3718297e871827ece
Tony Whyman <tony.whyman@mccallumwhyman.com>
2014-11-24 11:33:17 +0000

    *** empty log message ***

---
+++ ibcontrols.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ibcontrols;
+
+interface
+
+uses
+  IBLookupComboEditBox, IBDynamicGrid, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ibcontrols', @Register);
+end.
