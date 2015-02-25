12fdfc8323f7d5ec4c54da274328d788fe9df5fb
Tony Whyman <tony.whyman@mccallumwhyman.com>
2015-02-25 12:12:27 +0000

    *** empty log message ***

--- runtime/IBXConst.pas
+++ runtime/IBXConst.pas
@@ -174,7 +174,7 @@ resourcestring
   SInvalidOnErrorResult = 'Unexpected onError return value';
   SInvalidOnStatusResult = 'Unexpected onStatus return value';
 
-  SInterbaseExpressVersion = 'Firebird Express for Lazarus 1.1.0';
+  SInterbaseExpressVersion = 'Firebird Express for Lazarus 1.2.0';
   SEditSQL = 'Edit SQL';
   SIBSQLEditor = 'IBSQL Editor';
   SIBServiceEditor = 'Edit IB Service';
