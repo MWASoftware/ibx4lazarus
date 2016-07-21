fb113e853148b9dff58d8181f2fed3dc7f6c29aa
Tony Whyman <tony.whyman@mccallumwhyman.com>
2016-07-21 16:24:49 +0000

    *** empty log message ***

--- runtime/IBHeader.pas
+++ runtime/IBHeader.pas
@@ -116,18 +116,6 @@ type
   end;
   PISC_TIMESTAMP = ^TISC_TIMESTAMP;
 
-  (*********************************************************************)
-  (** Blob id structure                                               **)
-  (*********************************************************************)
-  TGDS_QUAD = record
-    gds_quad_high      : ISC_LONG;
-    gds_quad_low       : UISC_LONG;
-  end;
-  TGDS__QUAD           = TGDS_QUAD;
-  TISC_QUAD            = TGDS_QUAD;
-  PGDS_QUAD            = ^TGDS_QUAD;
-  PGDS__QUAD           = ^TGDS__QUAD;
-  PISC_QUAD            = ^TISC_QUAD;
 
   TISC_ARRAY_BOUND = record
     array_bound_lower  : short;
