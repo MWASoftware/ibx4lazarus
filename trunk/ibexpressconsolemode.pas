03845138c1403ced22b131f8a6c63cd60be85d51
Tony Whyman <tony.whyman@mccallumwhyman.com>
2016-09-22 14:25:29 +0000

    Release 1-4-2

---
+++ trunk/ibexpressconsolemode.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ibexpressconsolemode;
+
+interface
+
+uses
+  IB, IBBatchMove, IBBlob, IBCustomDataSet, IBDatabase, IBErrorCodes, 
+  IBEvents, IBExternals, IBExtract, IBHeader, IBUtils, IBXConst, IBIntf, 
+  IBQuery, IBServices, IBSQL, IBStoredProc, IBDatabaseInfo, IBUpdateSQL, 
+  IBTable, IBSQLMonitor, IBSQLParser, ibxscript, IBCMLocalDBSupport;
+
+implementation
+
+end.
