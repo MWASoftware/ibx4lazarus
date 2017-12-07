ae9dc639ac9ef64626c3d9216c32e9e82dfd7b5d
Tony Whyman <tony.whyman@mccallumwhyman.com>
2017-12-07 09:25:00 +0000

    Added tool buttons to SQL Editor

--- design/IBSQLEditFrame.pas
+++ design/IBSQLEditFrame.pas
@@ -6,15 +6,34 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, SynEdit, SynHighlighterSQL, LResources, Forms,
-  Controls, ActnList, Menus, LazSynTextArea;
+  Controls, ActnList, Menus, Dialogs, ComCtrls, LazSynTextArea;
 
 type
 
   { TIBSQLEditFrame }
 
   TIBSQLEditFrame = class(TFrame)
+    Redo: TAction;
+    ToolButton10: TToolButton;
+    ToolButton11: TToolButton;
+    Undo: TAction;
+    SaveToFile: TAction;
+    LoadFromFile: TAction;
+    BtnImages: TImageList;
     MenuItem8: TMenuItem;
     MenuItem9: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    ToolBar1: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+    ToolButton5: TToolButton;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ToolButton9: TToolButton;
     WrapText: TAction;
     Clear: TAction;
     MenuItem1: TMenuItem;
@@ -36,9 +55,16 @@ type
     procedure CopyTextExecute(Sender: TObject);
     procedure CutExecute(Sender: TObject);
     procedure CutUpdate(Sender: TObject);
+    procedure LoadFromFileExecute(Sender: TObject);
     procedure PasteExecute(Sender: TObject);
     procedure PasteUpdate(Sender: TObject);
+    procedure RedoExecute(Sender: TObject);
+    procedure RedoUpdate(Sender: TObject);
+    procedure SaveToFileExecute(Sender: TObject);
     procedure SelectAllExecute(Sender: TObject);
+    procedure SelectAllUpdate(Sender: TObject);
+    procedure UndoExecute(Sender: TObject);
+    procedure UndoUpdate(Sender: TObject);
     procedure WrapTextExecute(Sender: TObject);
     procedure WrapTextUpdate(Sender: TObject);
   private
@@ -60,6 +86,12 @@ begin
   (Sender as TAction).Enabled := SQLText.SelText <> '';
 end;
 
+procedure TIBSQLEditFrame.LoadFromFileExecute(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+    SQLText.Lines.LoadFromFile(OpenDialog1.FileName);
+end;
+
 procedure TIBSQLEditFrame.PasteExecute(Sender: TObject);
 begin
   SQLText.PasteFromClipboard;
@@ -70,9 +102,40 @@ begin
   (Sender as TAction).Enabled := SQLText.CanPaste;
 end;
 
+procedure TIBSQLEditFrame.RedoExecute(Sender: TObject);
+begin
+  SQLText.Redo;
+end;
+
+procedure TIBSQLEditFrame.RedoUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := SQLText.CanRedo;
+end;
+
+procedure TIBSQLEditFrame.SaveToFileExecute(Sender: TObject);
+begin
+  if SaveDialog1.Execute then
+    SQLText.Lines.SaveToFile(SaveDialog1.FileName);
+end;
+
 procedure TIBSQLEditFrame.SelectAllExecute(Sender: TObject);
 begin
   SQLText.SelectAll;
+end;
+
+procedure TIBSQLEditFrame.SelectAllUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := SQLText.Lines.Count > 0;
+end;
+
+procedure TIBSQLEditFrame.UndoExecute(Sender: TObject);
+begin
+  SQLText.Undo;
+end;
+
+procedure TIBSQLEditFrame.UndoUpdate(Sender: TObject);
+begin
+  (Sender as TAction).Enabled := SQLText.CanUndo;
 end;
 
 procedure TIBSQLEditFrame.WrapTextExecute(Sender: TObject);
