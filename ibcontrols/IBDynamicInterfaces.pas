ac96fe88a75a5d4bd875380276b68b6d73d7e6b1
Tony Whyman <tony.whyman@mccallumwhyman.com>
2024-05-23 14:35:27 +0000

    ibinterfaces split

---
+++ ibcontrols/IBDynamicInterfaces.pas
@@ -0,0 +1,18 @@
+unit IBDynamicInterfaces;
+
+{$mode objfpc}{$H+}
+{$interfaces corba}
+
+interface
+
+uses
+  Classes , SysUtils, DB;
+
+{$I ../include/IBDynamicInterfacesh.inc}
+
+implementation
+
+{$I ../include/IBDynamicInterfaces.inc}
+
+end.
+
