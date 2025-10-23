5bad6c00e339352e6d05326dd67796211691401c
tony <tony.whyman@mccallumwhyman.com>
2025-10-23 16:51:47 +0100

    Replace symlinks in testsuite/resources with proper files.

--- testsuite/resources/Test20.sql
+++ testsuite/resources/Test20.sql
@@ -1,1 +1,22 @@
-Test17.sql
+Create Table IBDataSetTest (
+  KeyField Integer not null,
+  PlainText VarChar(128),
+  TextAndKey Computed By (KeyField || ': ' || PlainText),
+  ServerSideText VarChar(256),
+  Primary Key (KeyField)
+);
+
+Create Generator AGenerator;
+
+Create Trigger BeforeInsertTest for IBDataSetTest
+Before Insert or Update
+As
+Begin
+  new.ServerSideText = new.KeyField || ' - ' || new.PlainText;
+End
+
+Create Table IBXTest2 (
+ Key1 integer not null,
+ Key2 integer not null,
+ primary key(key1,key2)
+);
--- testsuite/resources/Test29.sql
+++ testsuite/resources/Test29.sql
@@ -1,1 +1,27 @@
-Test04.sql
+Create Table IBXTest (
+ TableKey Integer not null,
+ F1 Smallint,
+ F2 Float,
+ F3 Double Precision,
+ F4 Decimal(9,3),
+ F5 Decimal(18,4),
+ F6 Date,
+ F7 Timestamp,
+ F8 Char (2),
+ F9 VarChar(256),
+ F10 Blob,
+ F11 BigInt,
+ "f12" Integer,
+ F13 Time,
+ F14 Blob sub_type text,
+ F15 Double Precision Computed by (F2 + F3),
+ MyArray Integer [0:16],
+ "GRANTS" VarChar(20),
+ "My Field" VarChar(32),
+ "MY Field" VarChar(32),
+ Primary Key(TableKey)
+);
+
+Create Generator IBXGen;
+
+
